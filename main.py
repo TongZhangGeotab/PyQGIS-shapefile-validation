1a0ae6239abe8b474e16cb9360dd0215a44a2de4
TongZhangGeotab <tongguangzhang@geotab.com>
2024-04-01 12:33:22 -0400

    wrote main script

---
+++ main.py
@@ -0,0 +1,40 @@
+import os
+
+from modules import fields
+from modules import intersections
+from modules import lengths
+
+FILE_PATH = os.path.expanduser('~/Downloads/LasVegas_Demo_Data/LasVegas_Route.shp')
+LAYER = QgsVectorLayer(FILE_PATH, '', 'ogr')
+
+SOURCE_CRS = QgsCoordinateReferenceSystem(4326)
+DEST_CRS = QgsCoordinateReferenceSystem(3395)
+TRANSFORM = QgsCoordinateTransform(SOURCE_CRS, DEST_CRS, QgsProject.instance())
+MAX_BOUND = 300
+MIN_BOUND = 100
+
+def main():
+    error_message = ''
+
+    fieldCheck = fields.Fields(LAYER)
+    fieldCheck.run()
+    error_message += fieldCheck.getErrorMessage()
+
+    intersectionCheck = intersections.Intersections(LAYER)
+    intersectionCheck.run()
+    error_message += intersectionCheck.getErrorMessage()
+
+    lengthCheck = lengths.Lengths(
+        layer=LAYER,
+        transform=TRANSFORM,
+        min_bound=MIN_BOUND,
+        max_bound=MAX_BOUND,
+    )
+    lengthCheck.run()
+    error_message += lengthCheck.getErrorMessage()
+
+    return error_message
+
+error_message = main()
+if error_message:
+    print(error_message)
