c27086647cf6b85dce6634b05585e26f67535fdd
TongZhangGeotab <tongguangzhang@geotab.com>
2024-04-04 14:07:06 -0400

    changed logger utility to take date input

--- utilities/configure_logger.py
+++ utilities/configure_logger.py
@@ -3,21 +3,27 @@ import logging
 import os
 
 
-def configure_logger(root_directory_path):
-    CURRENT_TIME = datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
-    LOG_FILE_NAME = os.path.join(root_directory_path, 'logs', f"{CURRENT_TIME}.log")
-    LOGGING_LEVEL = logging.DEBUG
+LOGGING_LEVEL = logging.DEBUG
+LOGGING_LEVEL_STREAM = logging.WARNING
 
-    file_handler = logging.FileHandler(filename=LOG_FILE_NAME, mode="w")
-    stream_handler = logging.StreamHandler()
+def configure_logger(root_directory_path, time):
+    log_file_name = os.path.join(root_directory_path, 'logs', f"{time}.log")
+
+    logger = logging.getLogger("QGIS_logger")
+    logger.setLevel(LOGGING_LEVEL)
+
     formatter = logging.Formatter(
         fmt="%(asctime)s: [%(levelname)s] %(message)s",
         datefmt="%I:%M:%S"
     )
+
+    file_handler = logging.FileHandler(filename=log_file_name, mode="w")
+    file_handler.setLevel(LOGGING_LEVEL)
     file_handler.setFormatter(formatter)
+
+    stream_handler = logging.StreamHandler()
+    stream_handler.setLevel(LOGGING_LEVEL_STREAM)
     stream_handler.setFormatter(formatter)
 
-    logger = logging.getLogger("QGIS_logger")
-    logger.setLevel(LOGGING_LEVEL)
     logger.addHandler(file_handler)
     logger.addHandler(stream_handler)
