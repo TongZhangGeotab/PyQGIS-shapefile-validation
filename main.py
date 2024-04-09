5567f012c63125e2cacc8c548f14a1db4d5c739b
TongZhangGeotab <tongguangzhang@geotab.com>
2024-04-09 12:37:05 -0400

    Added docstrings to main

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+"""
+Main script that runs in the QGIS environment
+"""
+
 import datetime
 import logging
 import os
@@ -8,36 +12,42 @@ from modules import intersections
 from modules import lengths
 from utilities import configure_logger
 
-FILE_PATH = os.path.expanduser("~/Downloads/1/PW Scenarios - 1.shp")
+# Upload layer and update project instance
+FILE_PATH = os.path.expanduser("~/Downloads/2/PW Scenarios - 2.shp")
 LAYER = QgsVectorLayer(FILE_PATH, "", "ogr")
 iface.addVectorLayer(FILE_PATH, "", "ogr")
 QCoreApplication.processEvents()
 
+# Constants for coordinates
 CRS = "EPSG:4326"
 INSTANCE = QgsProject.instance()
 
+# Spatial index for checking intersections
 INDEX = QgsSpatialIndex()
 
+# Constants for min and max segment lengths
 MIN_BOUND = 20
 MAX_BOUND = 1000
 
+# Setup for distance measuring object
 DISTANCE_AREA = QgsDistanceArea()
 DISTANCE_AREA.setEllipsoid("WGS84")
-CRS = QgsCoordinateReferenceSystem("EPSG:4326")
+CRS = QgsCoordinateReferenceSystem(CRS)
 DISTANCE_AREA.setSourceCrs(CRS, QgsProject.instance().transformContext())
 
+# Configure a logger
 configure_logger.configure_logger(
     os.path.dirname(os.path.realpath(__file__)),
     datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S"),
 )
+logger = logging.getLogger("QGIS_logger")
 
 
 def main():
-    logger = logging.getLogger("QGIS_logger")
-
     feedback_message = ""
     null_geoms = set()
 
+    # Check the attribute table
     fieldCheck = fields.Fields(layer=LAYER)
     fieldCheck.run()
 
@@ -45,6 +55,7 @@ def main():
     if result:
         feedback_message += message
 
+    # Check the coordinate system
     coordinateCheck = coordinates.Coordinates(
         layer=LAYER,
         correct_crs=CRS,
@@ -56,6 +67,7 @@ def main():
     if result:
         feedback_message += message
 
+    # Check for intersections
     intersectionCheck = intersections.Intersections(
         layer=LAYER,
         index=INDEX,
@@ -67,6 +79,7 @@ def main():
     if result:
         feedback_message += message
 
+    # Check the lengths
     lengthCheck = lengths.Lengths(
         layer=LAYER,
         min_bound=MIN_BOUND,
@@ -80,6 +93,7 @@ def main():
     if result:
         feedback_message += message
 
+    # Log geometries with Null values
     if null_geoms:
         logger.error(f"{len(null_geoms)} features have Null geometry")
         logger.info(f"features with Null geometry: {null_geoms}")
